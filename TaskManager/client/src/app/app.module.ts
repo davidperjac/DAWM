967bc1b9f8949c6b7d5c7e668853688686384eca
davidperjac <diperez@espol.edu.ec>
2022-08-05 00:09:51 -0500

    begin boards page and styles

--- TaskManager/client/src/app/app.module.ts
+++ TaskManager/client/src/app/app.module.ts
@@ -17,7 +17,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent,
@@ -34,6 +36,9 @@ import { MatInputModule } from '@angular/material/input';
     BrowserAnimationsModule,
     MatButtonModule,
     MatInputModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
     FormsModule,
     ReactiveFormsModule,
   ],
--- TaskManager/client/src/app/pages/boards/boards.component.ts
+++ TaskManager/client/src/app/pages/boards/boards.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Board } from 'src/app/models/board';
 
 @Component({
   selector: 'app-boards',
   templateUrl: './boards.component.html',
-  styleUrls: ['./boards.component.css']
+  styleUrls: ['./boards.component.css'],
 })
 export class BoardsComponent implements OnInit {
+  boards: Board[] = [
+    {
+      id: 'board1',
+      name: 'Work',
+      description: 'Workflow',
+      completed: 5,
+    },
+    {
+      id: 'board2',
+      name: 'Code',
+      description: 'React,Angular,Vue',
+      completed: 2,
+    },
+    {
+      id: 'board3',
+      name: 'Espol',
+      description: 'DAWM',
+      completed: 3,
+    },
+    {
+      id: 'board4',
+      name: 'Home',
+      description: 'Soccer match',
+      completed: 6,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
