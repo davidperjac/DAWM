98874d27c73d3b0981cefdcf9c999ab8ae8d918e
davidperjac <diperez@espol.edu.ec>
2022-08-04 23:19:49 -0500

    add signup page and styles

--- TaskManager/client/src/app/pages/signup/signup.component.ts
+++ TaskManager/client/src/app/pages/signup/signup.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
-  constructor() { }
+  passwordFormControl = new FormControl('', [Validators.required]);
+  confirmpasswordFormControl = new FormControl('', [Validators.required]);
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
