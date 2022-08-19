911b6205f8db8700af01be74d462b9ade43e0f79
davidperjac <diperez@espol.edu.ec>
2022-08-19 16:58:11 -0500

    change client title and start with server render documentation

--- TaskManager/server/src/v1/routes/index.js
+++ TaskManager/server/src/v1/routes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* RENDER HOME PAGE. */
-router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+	res.render('index');
 });
 
 /* ROUTES. */
